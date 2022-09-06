c1474de4260fa4e3f8a8c5ffe583bc6b2ff11da5
Ryan Uselmann <siriusscaper.dev@gmail.com>
2022-09-05 22:22:16 -0700

    Revert "new pull request"

--- routes/chose.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const todosController = require("../controllers/chose");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
-
-router.get("/", todosController.getIndex);
-router.post("/createDG", todosController.createDG);
-
-module.exports = router;
